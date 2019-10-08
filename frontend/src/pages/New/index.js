76635bfe6ade1e761ea6f86a7603df53acca81b1
Jecé Xavier <jecexavier@gmail.com>
2019-10-07 21:48:36 -0300

    Adição do upload de imagem com preview

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,24 +1,56 @@
 // Imports
-import React, {useState} from 'react';
+import React, {useState, useMemo} from 'react';
+import api from '../../services/api'
+import camera from '../../assets/camera.svg';
+import './style.css';
 
 // Componentes
-export default function New(){
-    const {company, setCompany} = useState('');
-    const {techs, setTechs} = useState('');
-    const {price, setPrice} = useState('');
+export default function New({history}){
+    const [thumbnail, setThumbnail] = useState(null);
+    const [techs, setTechs] = useState('');
+    const [price, setPrice] = useState('');
+    const [company, setCompany] = useState('');
+    
+    const preview = useMemo(() => {
+        return thumbnail ? URL.createObjectURL(thumbnail) : null
+    }, [thumbnail]);
 
-    function handleSubmit(){
+    async function handleSubmit(event){
+        event.preventDefault();
 
+        const data = new FormData();
+        const user_id = localStorage.getItem('user');
+
+        data.append('thumbnail', thumbnail);
+        data.append('company', company);
+        data.append('techs', techs);
+        data.append('price', price);
+        
+        await api.post('/spots', data, {
+            headers: {user_id}
+        });
+
+        history.push('/dashboard');
     }
 
     return (
         <form onSubmit={handleSubmit}>
+            <label 
+                id="thumbnail" 
+                style={{backgroundImage: `url(${preview})`}}
+                className={thumbnail ? 'has-thumbnail' : '' }
+            >
+                <input type="file" onChange={event => setThumbnail(event.target.files[0])} />
+                <img src={camera} alt="Select img"/>
+            </label>
+
             <label htmlFor="company">EMPRESA *</label>
             <input 
                 id="company"
                 placeholder="Sua empresa incrivel"
                 value={company}
                 onChange={event => setCompany(event.target.value)}
+                required
             />
 
             <label htmlFor="techs">TECNOLOGIAS * <span>(separadas por virgula)</span></label>
@@ -27,6 +59,7 @@ export default function New(){
                 placeholder="Quais tecnoogias usam?"
                 value={techs}
                 onChange={event => setTechs(event.target.value)}
+                required
             />
 
             <label htmlFor="price">VALOR DA DIÁRIA * <span>(em branco para GRATUITO)</span></label>
